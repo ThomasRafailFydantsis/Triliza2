43406d37a870c5e385990caae532cf10eccbca05
ThomasRafailFydantsis <thomasrafydantsis@hotmail.com>
2025-05-02 17:11:31 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -84,7 +84,11 @@ internal class Program
   
         builder.Services.AddAuthorization();
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers.AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+            options.JsonSerializerOptions.WriteIndented = true;
+        });
         builder.Logging.ClearProviders();
         builder.Logging.AddConsole();
 
